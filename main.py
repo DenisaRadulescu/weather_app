ef508f7140677fbda613db2c5bfebd34021ce5e0
DenisaRadulescu <denisa.r95@gmail.com>
2024-04-07 20:14:43 +0300

    add city info summary

--- main.py
+++ main.py
@@ -19,3 +19,5 @@ if __name__ == '__main__':
     print(key)
     weather_dict = weather.get_current_weather(url=config['url'], key=key, city="Cluj")
     print(json.dumps(weather_dict, indent=4))
+    city_facts = weather.city_info(url=config['url'], key=key, city="Cluj")
+    print(f"In {city_facts[1]}")
--- weather.py
+++ weather.py
@@ -17,3 +17,16 @@ def get_current_weather(url:str, key: str, city: str = "Bucharest"):
                             f"Message: {response.text}")
     except Exception as e:
         print(e)
+
+
+def city_info(url:str, key: str, city: str = "Bucharest"):
+    try:
+        response = requests.get(url + city, headers={"key": key})
+        if response.status_code == 200:
+            weather_dict = json.loads(response.text)
+            curr_temp = weather_dict['current']['temp_c']
+            curr_status = weather_dict['current']['condition']['text']
+            curr_hour = weather_dict['location']['localtime']
+            return curr_temp, curr_status, curr_hour
+    except Exception as e:
+        print(e)
